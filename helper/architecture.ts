d4fb45d4693ca3ebcbe3201109c77253c1371cf8
trustlelab <trustlelab@gmail.com>
2023-11-18 23:51:12 +0100

    feat: Currency helper-function

--- helper/architecture.ts
+++ helper/architecture.ts
@@ -5,3 +5,12 @@ export function handleEmptyString(input: any){
 export function handleEmptyArray(input: any){
     return (input)? input: [];
 }
+
+export function convertToCurrency(val: number){
+    let euro = new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+    });
+
+    return euro.format(val);
+}

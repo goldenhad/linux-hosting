30892782998728a43e27bca4ab7382dc6234480f
trustlelab <trustlelab@gmail.com>
2024-01-09 23:46:53 +0100

    fix: Changed some text

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -680,7 +680,7 @@ Register.getLayout = ( page ) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Siteware business dein intelligenter Mail-Assistent" />
+        <meta property="og:title" content="Siteware business dein intelligenter KI-Assistent" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/ogimage.jpg" />
         <meta property="og:url" content={`${process.env.BASEURL}`} />

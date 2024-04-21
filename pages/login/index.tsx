84d4df7788b367c1530d98ce2b249eac7c99b6b6
trustlelab <trustlelab@gmail.com>
2024-04-21 10:14:07 +0200

    chore: Changed indent and converted german comments to english

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -7,14 +7,12 @@ import Head from "next/head";
 import Link from "next/link";
 import CookieBanner from "../../components/CookieBanner/CookieBanner";
 import FatButton from "../../components/FatButton";
-import Nav from "../../public/icons/nav.svg";
-import Icon from "@ant-design/icons";
 import { logEvent } from "firebase/analytics";
 import { analytics } from "../../db";
 import getDocument from "../../firebase/data/getData";
 import { User } from "../../firebase/types/User";
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 const TypewriterEffect = ({ texts }) => {
   const [index, setIndex] = useState(0);
@@ -26,30 +24,30 @@ const TypewriterEffect = ({ texts }) => {
     if (index >= texts.length) return;
 
     if (subIndex === texts[index].length + 1 && !reverse) {
-        // Nach dem Anzeigen des gesamten Textes warten, bevor gelöscht wird
-        setTimeout(() => setReverse(true), 3000); // Warten Sie 3 Sekunden, bevor der Text gelöscht wird.
-        return;
+      // Wait after showing the text until it was deleted entirely
+      setTimeout(() => setReverse(true), 3000); // wait 3 seconds bevor deleting the text
+      return;
     }
 
     if (subIndex === 0 && reverse) {
-        // Sofortiger Reset des subIndex, um den Text zu löschen
-        setReverse(false);
-        setIndex((prev) => (prev + 1) % texts.length);
-        return;
+      // Reset the subindex to delete the text
+      setReverse(false);
+      setIndex((prev) => (prev + 1) % texts.length);
+      return;
     }
 
     const typingSpeed = 35; // Geschwindigkeit des Schreibens
     const timeout = setTimeout(() => {
-        setSubIndex(prev => reverse ? 0 : prev + 1);
+      setSubIndex((prev) => reverse ? 0 : prev + 1);
     }, typingSpeed);
 
     return () => clearTimeout(timeout);
   }, [subIndex, index, reverse, texts]);
 
   useEffect(() => {
-    const blinkSpeed = 500; // Geschwindigkeit des Blinkens beibehalten
+    const blinkSpeed = 500; // Speed of the blinking animation
     const timeout2 = setTimeout(() => {
-        setBlink((prev) => !prev);
+      setBlink((prev) => !prev);
     }, blinkSpeed);
 
     return () => clearTimeout(timeout2);
@@ -70,7 +68,7 @@ const frontendnav: MenuProps["items"] = [
   {
     label: <Link href={"legal"}>Impressum</Link>,
     key: "legal"
-  },
+  }
 ]
 
 /**
@@ -136,116 +134,116 @@ export default function Login(){
     
 
     <Layout className={styles.backgroundStyle}>
-    <div className={styles.overlayStyle}></div>
-    <div className={styles.overlay}></div>
-    <Content className={styles.contentContainer}>
-        <Row justify="space-around" align="bottom" style={{ height: '100%' , padding: '5% 0' }}>
-            <Col xs={8}  // Vollbreite auf sehr kleinen Bildschirmen
-      sm={8}  // Breitere Bildschirme, aber kleiner als Tablet
-      md={8}  // Tablet
-      lg={8}  // Kleine Desktops
-      xl={4}   // Große Desktops 
-      align="center" style={{ textAlign: 'right' }}>
+      <div className={styles.overlayStyle}></div>
+      <div className={styles.overlay}></div>
+      <Content className={styles.contentContainer}>
+        <Row justify="space-around" align="bottom" style={{ height: "100%" , padding: "5% 0" }}>
+          <Col xs={8}  // Fullwidth on very small screens
+            sm={8}  // Wider screens, but still smaller than tablets
+            md={8}  // Tablet
+            lg={8}  // Small Desktops
+            xl={4}   // Big Desktops
+            style={{ textAlign: "right" }}>
             <div className={styles.logobox}>
-            <Link href={"/login"}>
-              {/*eslint-disable-next-line */}
+              <Link href={"/login"}>
+                {/*eslint-disable-next-line */}
               <img src={"/siteware-logo-white.svg"} alt="Logo"/>
-            </Link>
-          </div>
-            </Col>
-            <Col xs={9}  // Vollbreite auf sehr kleinen Bildschirmen
-      sm={18}  // Breitere Bildschirme, aber kleiner als Tablet
-      md={12}  // Tablet
-      lg={10}  // Kleine Desktops
-      xl={8}   // Große Desktops 
-      >
-                <div className={styles.typewriterEffect}>
-                    <
-        TypewriterEffect texts = {
-            ["Ich bin Dein neuer KI Assistent aus Deutschland.",
-            "Ich biete Dir laufend neue KI -Apps, die das Leben leichter machen schreibe und antworte auf E-Mails in Deinem Stil.",
-            "Ich verfasse Text in dem Stil, den Du Dir wünschst.",
-            "Meine Ergebnisse sind  unlimitiert im Umfang.",
-            "Ich arbeite mit mehreren KI Modellen in einem System DMSP:", 
-            "Ich arbeite Deine Aufgaben in mehreren Schritten ab (DMSP).",
-            "Ich sorge dafür, dass Du wieder mehr Zeit für Deine wesentlichen Aufgaben hast.",
-            "Ich beantworte dir alle Fragen zu Excel wie ein Berater.",
-            "Ich biete Dir Zugang zu führenden KI Modellen in einem System.",
-            "Ich lerne schnell und viel über Dich, Dein Unternehmen, Deine Produkte und Dienstleistungen.",
-            "Ich übersetze Deine Texte in viele Sprachen.",
-            "Ich sorge dafür, dass Deine Texte der Rechtschreibung entsprechen.",
-            "Bald kannst Du eigene Apps mit Siteware entwickeln und damit Geld verdienen."
-            ]
-        }
-        />
-                </div>
-            </Col>
-            <Col className={styles.formContainer} xs={24}  // Vollbreite auf sehr kleinen Bildschirmen
-      sm={18}  // Breitere Bildschirme, aber kleiner als Tablet
-      md={12}  // Tablet
-      lg={10}  // Kleine Desktops
-      xl={8}   // Große Desktops 
-      >
-          <h1 className={styles.formtitle}>Log in</h1>
-          <div className={styles.formexplanation}>Willkommen zurück. Bitte gebe unten deine Logindaten ein.</div>
-          <Form
-            name="basic"
-            className={styles.loginform}
-            initialValues={{
-              remember: true
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            layout="vertical"
-            onChange={() => {
-              setLoginFailed( false ) 
-            }}
-          >
-            <Form.Item
-              label="E-Mail"
-              name="email"
-              className={styles.loginpart}
-            >
-              <Input className={styles.logininput} name="email" />
-            </Form.Item>
-
-            <Form.Item
-              label="Passwort"
-              name="password"
-              className={styles.loginpart}
-            >
-              <Input.Password className={styles.logininput}  name="password"/>
-            </Form.Item>
-
-            <div className={styles.rememberrow}>
-              <Checkbox name="remember">Eingeloggt bleiben</Checkbox>
-              <Link href={"/forgot/password"}>Passwort vergessen</Link>
+              </Link>
             </div>
+          </Col>
+          <Col xs={9}  // Fullwidth on very small screens
+            sm={18}  // Wider screens, but still smaller than tablets
+            md={12}  // Tablet
+            lg={10}  // Small Desktops
+            xl={8}   // Big Desktops
+          >
+            <div className={styles.typewriterEffect}>
+              <
+                TypewriterEffect texts = {
+                  ["Ich bin Dein neuer KI Assistent aus Deutschland.",
+                    "Ich biete Dir laufend neue KI -Apps, die das Leben leichter machen schreibe und antworte auf E-Mails in Deinem Stil.",
+                    "Ich verfasse Text in dem Stil, den Du Dir wünschst.",
+                    "Meine Ergebnisse sind  unlimitiert im Umfang.",
+                    "Ich arbeite mit mehreren KI Modellen in einem System DMSP:", 
+                    "Ich arbeite Deine Aufgaben in mehreren Schritten ab (DMSP).",
+                    "Ich sorge dafür, dass Du wieder mehr Zeit für Deine wesentlichen Aufgaben hast.",
+                    "Ich beantworte dir alle Fragen zu Excel wie ein Berater.",
+                    "Ich biete Dir Zugang zu führenden KI Modellen in einem System.",
+                    "Ich lerne schnell und viel über Dich, Dein Unternehmen, Deine Produkte und Dienstleistungen.",
+                    "Ich übersetze Deine Texte in viele Sprachen.",
+                    "Ich sorge dafür, dass Deine Texte der Rechtschreibung entsprechen.",
+                    "Bald kannst Du eigene Apps mit Siteware entwickeln und damit Geld verdienen."
+                  ]
+                }
+              />
+            </div>
+          </Col>
+          <Col className={styles.formContainer} xs={24}  // Vollbreite auf sehr kleinen Bildschirmen
+            sm={18}  // Fullwidth on very small screens
+            md={12}  // Tablet
+            lg={10}  // Small Desktops
+            xl={8}   // Big Desktops
+          >
+            <h1 className={styles.formtitle}>Log in</h1>
+            <div className={styles.formexplanation}>Willkommen zurück. Bitte gebe unten deine Logindaten ein.</div>
+            <Form
+              name="basic"
+              className={styles.loginform}
+              initialValues={{
+                remember: true
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+              layout="vertical"
+              onChange={() => {
+                setLoginFailed( false ) 
+              }}
+            >
+              <Form.Item
+                label="E-Mail"
+                name="email"
+                className={styles.loginpart}
+              >
+                <Input className={styles.logininput} name="email" />
+              </Form.Item>
 
-            <Alert
-              style={{ marginBottom: 20, marginTop: 20, display: ( loginFailed )? "block": "none" }}
-              message="Beim Anmelden ist etwas schief gelaufen bitte versuche es noch einmal!" 
-              type="error"
-            />
+              <Form.Item
+                label="Passwort"
+                name="password"
+                className={styles.loginpart}
+              >
+                <Input.Password className={styles.logininput}  name="password"/>
+              </Form.Item>
 
-            <Form.Item className={styles.loginbutton}>
-              <FatButton isSubmitButton={true} text="Kostenlos Anmelden" />
-            </Form.Item>
-          </Form>
+              <div className={styles.rememberrow}>
+                <Checkbox name="remember">Eingeloggt bleiben</Checkbox>
+                <Link href={"/forgot/password"}>Passwort vergessen</Link>
+              </div>
+
+              <Alert
+                style={{ marginBottom: 20, marginTop: 20, display: ( loginFailed )? "block": "none" }}
+                message="Beim Anmelden ist etwas schief gelaufen bitte versuche es noch einmal!" 
+                type="error"
+              />
+
+              <Form.Item className={styles.loginbutton}>
+                <FatButton isSubmitButton={true} text="Kostenlos Anmelden" />
+              </Form.Item>
+            </Form>
 
           
                     
-          <div className={styles.signupnotice}>
-            <div>Noch keinen Account?</div><Link className={styles.signuplink} href={"/register"}>Jetzt registrieren</Link>
-          </div>
-            </Col>
+            <div className={styles.signupnotice}>
+              <div>Noch keinen Account?</div><Link className={styles.signuplink} href={"/register"}>Jetzt registrieren</Link>
+            </div>
+          </Col>
         </Row>
-    </Content>
-    <Footer className={styles.footerContainer}>
-            <Menu className={styles.navmenu} mode="horizontal" items={frontendnav} />
+      </Content>
+      <Footer className={styles.footerContainer}>
+        <Menu className={styles.navmenu} mode="horizontal" items={frontendnav} />
             
-        </Footer>
+      </Footer>
     </Layout>
   );
 }

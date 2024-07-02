a4ff5c26cb2c75612c491805690610a657e7d3ee
trustlelab <trustlelab@gmail.com>
2024-07-02 21:45:10 +0530

    [add-react-chatbot-plugin-setup] setup of api

--- pages/api/chatbot/index.ts
+++ pages/api/chatbot/index.ts
@@ -1,16 +1,17 @@
 import { NextApiResponse, NextApiRequest } from "next";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+
 
 export default function handler(
   _req: NextApiRequest,
-  res: NextApiResponse<{}>,
+  res: NextApiResponse<object>
 ) {
-    let distPath = path.resolve(process.cwd(), 'chatbot', 'dist', 'chat.js');
-    let content = fs.readFileSync(distPath)
-    res.writeHead(200, {  
-        'Content-Type': 'text/javascript'
-    }); 
-    res.write(content);  
-    res.end();
+  const distPath = path.resolve(process.cwd(), "chatbot", "dist", "chat.js");
+  const content = fs.readFileSync(distPath)
+  res.writeHead(200, {
+    "Content-Type": "text/javascript"
+  });
+  res.write(content);
+  res.end();
 }

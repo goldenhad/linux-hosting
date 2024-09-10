001085358ac3df0ad4dc6566d42a53d0893a266b
trustlelab <trustlelab@gmail.com>
2024-09-11 01:03:11 +0530

    [add-react-chatbot-plugin-setup] added a way to autoload script

--- chatbot/src/services/index.ts
+++ chatbot/src/services/index.ts
@@ -1,10 +1,21 @@
-import axios from "axios";
+import axios, { type CreateAxiosDefaults } from "axios";
 
-const { APIKEY, AGENTID, BASEURL } = window.SITEWARE_CONFIG;
+const { APIKEY, AGENTID, BASEURL } = window.SITEWARE_CONFIG || {};
+
+const settings: CreateAxiosDefaults = {} as CreateAxiosDefaults;
+
+if(!BASEURL){
+  settings.baseURL = process.env.REACT_APP_BASEURL
+  settings.headers = {
+    "x-api-key": process.env.REACT_APP_APIKEY,
+    "x-assistant-id": process.env.REACT_APP_ASSISTANTID
+  }
+}
 export const API = axios.create({
   baseURL: BASEURL,
   headers: {
     "x-api-key": APIKEY,
     "x-assistant-id": AGENTID
-  }
+  },
+  ...settings
 })
